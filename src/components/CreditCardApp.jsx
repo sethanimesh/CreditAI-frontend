a6f7d7a97d7510b4db98af82442ca409d3ea9bd6
animesh <sethanimesh@hotmail.com>
2024-08-21 12:37:13 +0530

    registration component
    adding component to take new user registration and then doing a POST request:

--- src/components/CreditCardApp.jsx
+++ src/components/CreditCardApp.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginComponent from "./LoginComponent";
 import WelcomeComponent from "./WelcomeComponent";
 import ErrorComponent from "./ErrorComponent";
+import RegisterComponent from "./RegisterComponent";
 
 export default function CreditCardApp(){
     return (
         <>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/register" element={<RegisterComponent/>}></Route>
                     <Route path="/" element={<LoginComponent/>}></Route>
                     <Route path="/login" element={<LoginComponent/>}></Route>
                     <Route path="/welcome/:username" element={<WelcomeComponent/>}></Route>
