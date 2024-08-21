853398f4b7b1b2f267d6ac19ae994d1111816c18
animesh <sethanimesh@hotmail.com>
2024-08-21 11:37:24 +0530

    controlled component
    making the login componet as a controlled component which means that the values of the form are handled by react state

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react"
+
 export default function LoginComponent(){
+
+    const[username, setUsername] = useState("sethanimesh")
+    const[password, setPassword] = useState("")
+    
+    function handleUsernameChange(event){
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event){
+        setPassword(event.target.value)
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>Username</label>
-                    <input type="text" name="username"/>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password"/>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
                     <button type="button" name="login">Login</button>
