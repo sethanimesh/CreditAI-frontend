ee6b7e3036aa2cccc9b81b820012ab8ee5c9de81
animesh <sethanimesh@hotmail.com>
2024-08-21 11:53:53 +0530

    routing
    adding browser route to the main component to navigate to various components

--- src/components/CreditCardApp.jsx
+++ src/components/CreditCardApp.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginComponent from "./LoginComponent";
+import WelcomeComponent from "./WelcomeComponent";
 
 export default function CreditCardApp(){
     return (
-        <LoginComponent/>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginComponent/>}></Route>
+                    <Route path="/login" element={<LoginComponent/>}></Route>
+                    <Route path="/welcome" element={<WelcomeComponent/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
