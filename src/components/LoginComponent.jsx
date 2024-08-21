b2ce7de43dffcc057b315f8a006e8ec9ff90ebca
animesh <sethanimesh@hotmail.com>
2024-08-21 11:44:37 +0530

    handling hardcoded authentication
    providing error message and success message components and functinos to handle the authentication

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -4,7 +4,10 @@ export default function LoginComponent(){
 
     const[username, setUsername] = useState("sethanimesh")
     const[password, setPassword] = useState("")
-    
+
+    const[showSuccessMessage, setSuccessMessage] = useState(false)
+    const[showErrorMessage, setErrorMessage] = useState(false)
+
     function handleUsernameChange(event){
         setUsername(event.target.value)
     }
@@ -13,9 +16,42 @@ export default function LoginComponent(){
         setPassword(event.target.value)
     }
 
+    function handleSubmit(){
+        if ((username==="sethanimesh") && (password==="dummy123")){
+            setSuccessMessage(true)
+            setErrorMessage(false)
+        }
+        else{
+            setSuccessMessage(false)
+            setErrorMessage(true)
+        }
+    }
+
+    function SuccessMessageComponent(){
+        if (showSuccessMessage){
+            return (
+                <div className="successMessage">Authenticated Successfully</div>
+            )
+        }
+        return null
+    }
+
+    function ErrorMessageComponent(){
+        if (showErrorMessage){
+            return (
+                <div className="errorMessage">Authentication Failed</div>
+            )
+        }
+        return null
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
+                <div>
+                    <SuccessMessageComponent/>
+                    <ErrorMessageComponent/>
+                </div>
                 <div>
                     <label>Username</label>
                     <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
@@ -25,7 +61,7 @@ export default function LoginComponent(){
                     <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" name="login">Login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>Login</button>
                 </div>
             </div>
         </div>
